3d6f1c6fc0b19d3a007a8b21be57918f65420fe9
DeadSomeOne <118965440+DeadSomeOne@users.noreply.github.com>
2023-10-19 22:19:05 +0000

    DerpFest bringup
    Lineage-sdk folder removed and contents have been moved because derpfest doesn't support lineage-sdk
    Derpfest default camera is Flash

--- derp_pyxis.mk
+++ derp_pyxis.mk
@@ -8,13 +8,23 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common DerpFest stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+
+# Target Boot Animation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device UDFPS
+TARGET_HAS_UDFPS := true
+
+EXTRA_UDFPS_ANIMATIONS := true
+
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
 # Inherit from pyxis device
 $(call inherit-product, device/xiaomi/pyxis/device.mk)
 
-PRODUCT_NAME := lineage_pyxis
+PRODUCT_NAME := derp_pyxis
 PRODUCT_DEVICE := pyxis
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
