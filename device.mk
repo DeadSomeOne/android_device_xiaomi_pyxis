945d67a9f49a9fc9f0a3e7930c11ce2fd212083a
DeadSomeOne <118965440+DeadSomeOne@users.noreply.github.com>
2023-10-19 22:20:19 +0000

    Remove unwanted Google apps
    I don't need full gapps package, I'll tune it to my preference

--- device.mk
+++ device.mk
@@ -31,6 +31,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/qdcm/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
